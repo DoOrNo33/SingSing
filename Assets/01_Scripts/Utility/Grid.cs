29005e273d3669551ee7fe0d36cb238f162150cc
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-09-28 17:23:37 +0900

    [Feature] Grid System Basic

--- Assets/01_Scripts/Utility/Grid.cs
+++ Assets/01_Scripts/Utility/Grid.cs
@@ -23,9 +23,15 @@ public class Grid
         {
             for (int z = 0; z < gridArray.GetLength(1); z++)
             {
-                UtilsClass.CreateWorldText(gridArray[x, z].ToString(), null, GetWorldPosition(x, z), fontSize, Color.white, TextAnchor.MiddleCenter);
+                UtilsClass.CreateWorldText(gridArray[x, z].ToString(), null, GetWorldPosition(x, z) + new Vector3(cellSize, 0, cellSize) * .5f, fontSize, Color.white, TextAnchor.MiddleCenter);
+
+                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x + 1, z), Color.white, 100f);
             }
         }
+
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
     }
 
     private Vector3 GetWorldPosition(int x, int z)
