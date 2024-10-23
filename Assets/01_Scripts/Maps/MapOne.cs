549eab780b1538885bf0ee91bbc0d3c16f06d42d
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-10-23 18:25:43 +0900

    [Feature] Map

---
+++ Assets/01_Scripts/Maps/MapOne.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapOne : Maps
+{
+    // Null,
+    // Trap,
+    // Score,
+    // Reflection,
+    // Coin
+
+    public int[,] mapSave = new int[3, 3]
+    {
+        {0, 1, 1},
+        {1, 2, 2},
+        {3, 2, 2}
+    };
+}
