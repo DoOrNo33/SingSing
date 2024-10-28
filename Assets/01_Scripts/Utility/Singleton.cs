a93917d05ab9a41784ce12a48ac7158250d04baf
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-10-28 16:43:10 +0900

    [Feature] 싱글톤 생성

---
+++ Assets/01_Scripts/Utility/Singleton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject obj = new GameObject();
+                obj.name = typeof(T).Name;
+                instance = obj.AddComponent<T>();
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected void OnDestory()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+
+}
